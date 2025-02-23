575331e32123a3092a927936d0ad1b7ab2c9f435
Alexander Podstavkin <shurshick@yandex.ru>
2025-02-23 22:28:13 +0300

    doubling server

--- server.mjs
+++ server.mjs
@@ -1,11 +1,20 @@
 // server.mjs
 import { createServer } from 'node:http';
-const server = createServer((req, res) => {
+const serverone = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('This is ServerProgramOne[1]\n');
   res.end('Hello World!\n');
 });
-// starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+const servertwo = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('This is ServerProgramTwo[2]\n');
+  res.end('Hello World!\n');
+});
+// starts a simple http server's locally on port 3000, 3001
+serverone.listen(3000, '127.0.0.1', () => {
+  console.log('Server1 Listening on 127.0.0.1:3000');
+});
+servertwo.listen(3001, '127.0.0.1', () => {
+  console.log('Server2 Listening on 127.0.0.1:3001');
 });
 // run with `node server.mjs`
